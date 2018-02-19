55385c6a2928b4803115275116165e967a92dcbc
Gloria Jun <pureainu@gmail.com>
2018-02-20 01:18:17 +0900

    의존 라이브러리 import

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import Vue from 'vue'
 import App from './app/App.vue'
 import router from './router/index.js'
 
+/*
+ plugin
+ */
+import 'bootstrap'
+import 'admin-lte'
+
+/*
+  styles
+ */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'admin-lte/dist/css/AdminLTE.min.css'
+import 'admin-lte/dist/css/skins/_all-skins.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 new Vue({
   el: '#app',
